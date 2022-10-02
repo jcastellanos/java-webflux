93e751530ca93cdf8cef46b9e0cc7e0e9cf74f55
jccastellanos <jccastellanos@grupoasd.com.co>
2022-10-01 21:43:02 -0500

    Initial commit

---
+++ src/main/java/co/jcastellanos/WebfluxApplication.java
@@ -0,0 +1,13 @@
+package co.jcastellanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/jcastellanos/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jcastellanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
